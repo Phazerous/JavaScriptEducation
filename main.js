aba69fff79500469b01793616f7e142d3b1c351a
Phazerous <nphazerous@gmail.com>
2022-10-11 19:27:34 +0600

    Проект "Цензор"

--- main.js
+++ main.js
@@ -1,66 +1,30 @@
-// 1 \\
+function censor() {
+    var changes = [];
 
-const names = ['Mikhail', 'Ivan', 'Albert', 'John', 'Ian', 'Petr', 'Alexandr', 'Oleg', 'Jaroslav', 'Vsevolod'];
+    return function(a, b = 0) {
+        if (b != 0) {
+            changes.push([a, b]);
+            return;
+        }
 
-const shortNames = names.filter(name => name.length <= 5);
+        var result = a;
 
-// 2 \\
+        for (change of changes) {
+            if (a.includes(change[0])) {
+                result = result.replace(change[0], change[1]);
+            }
+        }
 
-const products = [
-    {
-        id: 1,
-        name: 'Intro to JavaScript',
-        category: 'books',
-        price: 150,
-    },
-    {
-        id: 2,
-        name: 'Intro to CSS',
-        category: 'books',
-        price: 120,
-    },
-    {
-        id: 3,
-        name: 'Deep dive into JavaScript',
-        category: 'books',
-        price: 350,
-    },
-    {
-        id: 4,
-        name: 'Intro to PHP',
-        category: 'books',
-        price: 250,
-    },
-    {
-        id: 5,
-        name: 'Deep dive into PHP',
-        category: 'books',
-        price: 350,
-    },
-];
-
-const cheapProducts = products.filter(product => product.price < 300);
-
-// 3 \\
-
-const developers = [
-    {
-        id: 1,
-        fullName: 'Ivan Ivanov',
-        skills: ['HTML', 'CSS', 'Git', 'Gulp', 'Pug']
-    },
-    {
-        id: 2,
-        fullName: 'Petr Petrov',
-        skills: ['HTML', 'CSS', 'Git', 'JavaScript', 'npm']
-    },
-    {
-        id: 3,
-        fullName: 'Ian Melnikov',
-        skills: ['HTML', 'CSS', 'Git', 'JavaScript', 'TypeScript']
+        return result;
     }
-];
 
-const tsDevelopers = developers.filter(dev => dev.skills.includes('TypeScript'));
 
-console.log(tsDevelopers);
+}
+
+const changeScene = censor();
+
+changeScene('PHP','JS');
+
+changeScene('backend', 'frontend')
+
+console.log(changeScene('PHP is the most popular programming language for backend web-development'));
