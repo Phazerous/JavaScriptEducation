d6a320c9df7d8e2c88d370d1c9dd12bfebfad0d6
Phazerous <nphazerous@gmail.com>
2022-10-13 10:37:00 +0600

    Дана коллекция players. Создайте новую переменную jsonPlayers и сохраните в ней оригинальную коллекцию в формате JSON.

--- main.js
+++ main.js
@@ -1,26 +1,29 @@
-const link = {
-    href: '#',
-    title: 'simple link',
-    target: 'blank',
-    className: 'link',
-    id: null,
-    children: {
-        span: {
-            content: 'Click me',
-            className: 'anchor',
-        },
+const players = [
+    {
+        id: 1,
+        name: 'Andres',
+        surname: 'Iniesta',
+        club: 'Vissel Cobe',
     },
-}
+    {
+        id: 2,
+        name: 'Eden',
+        surname: 'Hazard',
+        club: 'Real Madrid',
+    },
+    {
+        id: 3,
+        name: 'Mo',
+        surname: 'Salah',
+        club: 'Liverpool',
+    },
+    {
+        id: 4,
+        name: 'Lionel',
+        surname: 'Messi',
+        club: 'Barcelona',
+    },
+];
 
-const {
-    target,
-    className,
-    children: {
-        span: {
-            content,
-            className: renamed,
-        }
-    }
-} = link;
-
-console.log(target, className, content, renamed);
+const jsonPlayers = JSON.stringify(players);
+console.log(jsonPlayers);
