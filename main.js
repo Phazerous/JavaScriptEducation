8f7e8ef3094bf871cb364a4c0132fb7e95a3dbde
Phazerous <nphazerous@gmail.com>
2022-10-13 10:00:26 +0600

    Дан массив food и коллекция players. Создайте одну общую функцию сортировки, которая бы позволяла сортировать food по алфавиту от А-Я, а также игроков коллекции по ключу surname. И отсортируйте данные массивы, с помощью созданной функции.

--- main.js
+++ main.js
@@ -1,30 +1,51 @@
+function sortByString(a, b) {
+    if (a > b) {
+        return 1;
+    } else if (a < b) {
+        return - 1;
+    } else {
+        return 0;
+    }
+}
+
+const food = ["Apple", "Melon", "Banana", "Yogurt", "Orange", "Stawberry"];
+
 const players = [
     {
         id: 1,
-        name: 'Andres',
-        surname: 'Iniesta',
-        club: 'Vissel Cobe',
+        name: 'Cristiano',
+        surname: 'Ronaldo',
+        club: 'Juventus',
     },
     {
         id: 2,
-        name: 'Eden',
-        surname: 'Hazard',
-        club: 'Real Madrid',
-    },
-    {
-        id: 3,
-        name: 'Mo',
-        surname: 'Salah',
-        club: 'Liverpool',
-    },
-    {
-        id: 4,
         name: 'Lionel',
         surname: 'Messi',
         club: 'Barcelona',
     },
+    {
+        id: 3,
+        name: 'Karim',
+        surname: 'Benzema',
+        club: 'Real Madrid',
+    },
+    {
+        id: 4,
+        name: 'Maxi',
+        surname: 'Gomez',
+        club: 'Valencia',
+    },
+    {
+        id: 5,
+        name: 'Quincy',
+        surname: 'Promes',
+        club: 'Spartak',
+    },
 ];
 
-let messi = players.find(p => p.surname === 'Messi');
+food.sort((a, b) => sortByString(a, b));
 
-console.log(messi);
+players.sort((a, b) => sortByString(a.surname, b.surname));
+
+console.log(food);
+console.log(players);
