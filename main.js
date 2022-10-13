120a4291677ab72bc20ecdff777fbd828b6db832
Phazerous <nphazerous@gmail.com>
2022-10-13 10:06:52 +0600

    Дана функция getInfo, возвращающая массив в результате вызова. Создайте переменные carName и carSeries и при помощи деструктуризации назначьте ей значения, возвращаемые функцией.

--- main.js
+++ main.js
@@ -1,52 +1,8 @@
-function sortByString(a, b) {
-    if (a > b) {
-        return 1;
-    } else if (a < b) {
-        return - 1;
-    } else {
-        return 0;
-    }
+function getInfo() {
+    return ['BMW', 'X3'];
 }
 
-const food = ["Apple", "Melon", "Banana", "Yogurt", "Orange", "Stawberry"];
+let [carName, carSeries] = [...getInfo()];
 
-const players = [
-    {
-        id: 1,
-        name: 'Cristiano',
-        surname: 'Ronaldo',
-        club: 'Juventus',
-    },
-    {
-        id: 2,
-        name: 'Lionel',
-        surname: 'Messi',
-        club: 'Barcelona',
-    },
-    {
-        id: 3,
-        name: 'Karim',
-        surname: 'Benzema',
-        club: 'Real Madrid',
-    },
-    {
-        id: 4,
-        name: 'Maxi',
-        surname: 'Gomez',
-        club: 'Valencia',
-    },
-    {
-        id: 5,
-        name: 'Quincy',
-        surname: 'Promes',
-        club: 'Spartak',
-    },
-];
-
-food.sort((a, b) => sortByString(a, b));
-
-players.sort((a, b) => sortByString(a.surname, b.surname));
-
-console.log(food);
-console.log(players);
-console.log("Hello")
+console.log(carName);
+console.log(carSeries);
